75b0973887b807fdff863fe65cda716e0a212e65
yongrenwang <czie.alexmak@gmail.com>
2017-02-19 11:27:09 +0800

    Refactored file directory

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ var port = 3000;
 
 app.use("/",express.static('public'));
 
+app.get("/todos",function(req,res){
+	res.send('These are the todos!');
+});
 
 app.listen(port,function(){
 	console.log("The server is listening on port " + port + "!");
