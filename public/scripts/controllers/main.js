899af0dd62f7b4d6fbb2684793172d4527fc724c
yongrenwang <czie.alexmak@gmail.com>
2017-02-19 15:59:10 +0800

    Included Angular-wrapped Javascript APIs as services injected into the mainCtrl

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -1,8 +1,21 @@
 'use strict';
 
 angular.module('todoListApp')
-.controller('mainCtrl', function($scope, dataService){
+.controller('mainCtrl', function($scope, $log, $interval, dataService){
   
+  $scope.seconds = 0;
+
+  $scope.counter = function(){
+  	 $scope.seconds++;
+  	$log.log($scope.seconds + " have passed!");
+  	 $log.error($scope.seconds + " have passed!");
+  	 $log.warn($scope.seconds + " have passed!");
+
+
+  }
+
+  $interval($scope.counter,1000,10);
+
   dataService.getTodos(function(response){
     var todos = response.data;  
     $scope.todos =  todos;
