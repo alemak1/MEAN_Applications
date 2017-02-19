12cbff1243c43dc760098854c7a248ff0ef776ca
yongrenwang <czie.alexmak@gmail.com>
2017-02-19 11:22:47 +0800

    Specified static asset middleware with app.use(...), public folder is configured to be the directory from which app serves static assets

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ var express = require('express');
 var app = express();
 var port = 3000;
 
+app.use("/",express.static('public'));
+
+
 app.listen(port,function(){
 	console.log("The server is listening on port " + port + "!");
 });
