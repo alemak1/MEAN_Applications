17d26a300304843972c3eb08591bcaeebb155649
yongrenwang <czie.alexmak@gmail.com>
2017-02-19 11:46:03 +0800

    Refactor: code for instantiating router and configuring api routes is reloacted to the index.js folder, which is imported in the main app.js file and then mounted to the main express app by passing it in as argument to the use function

--- src/app.js
+++ src/app.js
@@ -1,17 +1,14 @@
 'use strict';
 
 var express = require('express');
+var router = require('./api');
 
 var app = express();
 var port = 3000;
 
 app.use("/",express.static('public'));
 
-var router = express.Router();
 
-router.get("/todos",function(req,res){
-	res.json({todos:[]});
-});
 
 app.use('/api',router);
 
